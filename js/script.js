f6428335137f2a3784e82a14596c7ab0e8afebf9
Ivan Mikerin <vmik7@yandex.ru>
2020-11-20 12:24:55 +0300

    Добвлена базовая структура проекта

---
+++ js/script.js
@@ -0,0 +1,6 @@
+"use strict"
+
+// Ширина и высота экрана
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
